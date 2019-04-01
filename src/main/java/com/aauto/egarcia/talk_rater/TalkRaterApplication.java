f90643fff5a5ae4154daef396867c9f1cd808d42
Eduardo García <lalinux@gmail.com>
2019-04-01 13:29:16 -0400

    #3 Initialize springboot app

---
+++ src/main/java/com/aauto/egarcia/talk_rater/TalkRaterApplication.java
@@ -0,0 +1,13 @@
+package com.aauto.egarcia.talk_rater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkRaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkRaterApplication.class, args);
+	}
+
+}
